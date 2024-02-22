4c25ecb1ec53307422aeba7a599edc595e2b01f3
ob-iwan <i.swillens@sintlucasedu.nl>
2024-02-22 09:45:31 +0100

    tilemap, key with door, chest

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ document.getElementById("mainTitle").innerText = "PointVenture";
 const gameWindow = document.getElementById("gameWindow");
 
 //inventory box
+let inventory = [];
 const inventoryList = document.getElementById("inventoryList")
 
 //main character
@@ -11,9 +12,9 @@ const hero = document.getElementById("hero");
 const offsetHero = 16;
 
 //things
-const tree1 = document.getElementById("tree");
-const coin1 = document.getElementById("coin");
-const key1 = document.getElementById("key");
+const tree = document.getElementById("tree");
+const coin = document.getElementById("coin");
+const key = document.getElementById("key");
 
 gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
@@ -21,30 +22,85 @@ gameWindow.onclick = function (e) {
     var y = e.clientY - rect.top;
 
     //offset so hero is in middle of mouse
-    if (e.traget.id != hero) {
+    if (e.target.id !== "heroImage") {
         hero.style.left = x - offsetHero + "px";
         hero.style.top = y - offsetHero + "px";
     }
-
     switch (e.target.id) {
-        case "tree":
-            tree1.style.opacity = 0.6;
-            break;
         case "coin":
-            coin1.remove();
-            const coinElement = document.createElement("li");
-            coinElement.id = "inv-coin";
-            coinElement.innerText = "coin";
-            inventoryList.appendChild(coinElement);
+            getItem("coin", "invCoin");
             break;
         case "key":
-            key1.remove();
-            const keyElement = document.createElement("li");
-            keyElement.id = "inv-key";
-            keyElement.innerText = "key";
-            inventoryList.appendChild(keyElement);
+            getItem("key", "invKey");
+            break;
+        case "chest":
+            getItem("sword", "invSword");
+            break;
+        case "chestDoor":
+            if (checkItem("key")) {
+                console.log("The door has opened :o");
+                removeItem("key" + "invKey");
+            }
+            else if (checkItem("coin")) {
+                console.log("You used a coin. \n"
+                    + "It wasn't very effective");
+            }
+            else {
+                console.log("sheiße the door is locked!");
+            }
+            break;
+        case "torch":
+            console.log("go away")
             break;
         default:
-            tree1.style.opacity = 1;
+            break;
+    }
+
+    /**
+     * checks if the value exists
+     * if not then it adds the value to the array and use showItem function
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    function getItem(itemName, itemId) {
+        if (!checkItem(itemName)) {
+            inventory.push(itemName);
+            showItem(itemName, itemId);
+        }
+    }
+
+    /**
+     * checks for certain item
+     * @param {string} itemName 
+     * @returns 
+     */
+    function checkItem(itemName) {
+        return inventory.includes(itemName);
+    }
+
+    /**
+     * needs name for display and an id
+     * @param {string} itemName
+     * @param {string} itemId
+     */
+    function showItem(itemName, itemId) {
+        console.log('you\'ve found a ' + itemName + '!')
+        const element = document.createElement("li");
+        element.id = itemId;
+        element.innerText = itemName;
+        inventoryList.appendChild(element);
+    }
+
+    /**
+     * removes item from array and the element from the HTML
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    function removeItem(itemName, itemId) {
+        //removes item in array
+        inventory = inventory.filter(function (newInventory) {
+            return newInventory !== itemName;
+        });
+        document.getElementById(itemId).remove();
     }
 }
