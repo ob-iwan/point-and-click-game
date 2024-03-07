b2de392effb716a775d9369d85d0be83b1d62704
ob-iwan <i.swillens@sintlucasedu.nl>
2024-03-07 19:47:52 +0100

    candles lightable and more sounds

--- js/main.js
+++ js/main.js
@@ -25,6 +25,10 @@ if (Storage) {
     }
 }
 
+//audio's
+const lighting = document.getElementById("lighting");
+const backgroundMusic = document.getElementById("backgroundMusic");
+
 //game window reference
 const gameWindow = document.getElementById("gameWindow");
 
@@ -49,6 +53,9 @@ const interactAvatar = document.getElementById("interactAvatar");
 const coin = document.getElementById("coin");
 const key = document.getElementById("key");
 
+//extra
+let playBackgroundMusic = true;
+
 if (gameState.keyPickedUp) {
     document.getElementById("key").remove();
 }
@@ -67,7 +74,18 @@ if (gameState.hasSword) {
 
 updateInventory(gameState.inventory, inventoryList);
 
+if (!playBackgroundMusic) {
+    backgroundMusic.pause();
+    backgroundMusic.currentTime = 0;
+}
+
 gameWindow.onclick = function (e) {
+    //makes the background music play and stop if needed
+    if (playBackgroundMusic) {
+        backgroundMusic.play();
+        backgroundMusic.loop = true;
+
+    }
     if (e.target.className !== "unclickable") {
         var rect = gameWindow.getBoundingClientRect();
         var x = e.clientX - rect.left;
@@ -113,7 +131,6 @@ gameWindow.onclick = function (e) {
                     if (checkItem("key")) {
                         showMessage(heroSpeech, "The door has opened :o", heroAudio);
                         document.getElementById("chestDoor").remove();
-                        document.getElementById("hiddenRoom").remove();
                         gameState.doorOpened = true;
                         saveGameState(gameState);
                     }
@@ -126,28 +143,62 @@ gameWindow.onclick = function (e) {
                     }
                     break;
                 case "priestImg":
-                    showMessage(interactSpeech, "Welcome to the dungeon, be aware of great monsters", interactAudio);
+                    interactSpeech.style.opacity = 1;
                     interactAvatar.style.opacity = 1;
-                    setTimeout(showMessage, 3000, heroSpeech, "Monsters?!", heroAudio);
-                    setTimeout(showMessage, 6000, interactSpeech, "Yea so what?", interactAudio);
-                    setTimeout(showMessage, 9000, heroSpeech, "I'm just so scared...", heroAudio);
-                    setTimeout(showMessage, 12000, interactSpeech, "What gives, just go and explore", interactAudio);
-                    setTimeout(function () { interactAvatar.style.opacity = 0; }, 15000);
-                    setTimeout(function () { priest.style.top = "576px" }, 16000)
-                    setTimeout(function () { priest.remove() }, 16500)
-                    setTimeout(function () { document.getElementById("hiddenRoom1").remove() }, 16500)
+                    setTimeout(showMessage, 1000, interactSpeech, "Welcome to the dungeon, be aware of great monsters", interactAudio)
+                    setTimeout(showMessage, 4000, heroSpeech, "Monsters?!", heroAudio);
+                    setTimeout(showMessage, 7000, interactSpeech, "Yea so what?", interactAudio);
+                    setTimeout(showMessage, 10000, heroSpeech, "I'm just so scared...", heroAudio);
+                    setTimeout(showMessage, 13000, interactSpeech, "What gives, just go and explore", interactAudio);
+                    setTimeout(function () { interactAvatar.style.opacity = 0; }, 16000);
+                    setTimeout(function () { priest.style.top = "576px" }, 17000)
+                    setTimeout(function () { priest.remove() }, 17500)
+                    setTimeout(function () { document.getElementById("hiddenRoom1").remove() }, 17500)
                     break;
                 case "priest":
-                    showMessage(interactSpeech, "Welcome to the dungeon, be aware of great monsters", interactAudio);
+                    interactSpeech.style.opacity = 1;
                     interactAvatar.style.opacity = 1;
-                    setTimeout(showMessage, 3000, heroSpeech, "Monsters?!", heroAudio);
-                    setTimeout(showMessage, 6000, interactSpeech, "Yea so what?", interactAudio);
-                    setTimeout(showMessage, 9000, heroSpeech, "I'm just so scared...", heroAudio);
-                    setTimeout(showMessage, 12000, interactSpeech, "What gives, just go and explore", interactAudio);
-                    setTimeout(function () { interactAvatar.style.opacity = 0; }, 15000);
-                    setTimeout(function () { priest.style.top = "576px" }, 16000)
-                    setTimeout(function () { priest.remove() }, 16500)
-                    setTimeout(function () { document.getElementById("hiddenRoom1").remove() }, 16500)
+                    setTimeout(showMessage, 1000, interactSpeech, "Welcome to the dungeon, be aware of great monsters", interactAudio)
+                    setTimeout(showMessage, 4000, heroSpeech, "Monsters?!", heroAudio);
+                    setTimeout(showMessage, 7000, interactSpeech, "Yea so what?", interactAudio);
+                    setTimeout(showMessage, 10000, heroSpeech, "I'm just so scared...", heroAudio);
+                    setTimeout(showMessage, 13000, interactSpeech, "What gives, just go and explore", interactAudio);
+                    setTimeout(function () { interactAvatar.style.opacity = 0; }, 16000);
+                    setTimeout(function () { priest.style.top = "576px" }, 17000)
+                    setTimeout(function () { priest.remove() }, 17500)
+                    setTimeout(function () { document.getElementById("hiddenRoom1").remove(); }, 17500)
+                    break;
+                case "standingTorch2Img":
+                    showMessage(heroSpeech, "You light the candle*", heroAudio);
+                    setTimeout(function () {
+                        lighting.currentTime = 0;
+                        document.getElementById("hiddenRoom2").remove();
+                        document.getElementById("hiddenRoom4").remove();
+                        document.getElementById("standingTorch2Img").remove();
+                        document.getElementById("standingTorchLighted2Img").style.opacity = 1;
+                        lighting.play();
+                    }, 1000);
+                    break;
+                case "standingTorch1Img":
+                    showMessage(heroSpeech, "You light another candle*", heroAudio);
+                    setTimeout(function () {
+                        lighting.currentTime = 0;
+                        document.getElementById("hiddenRoom5").remove();
+                        document.getElementById("hiddenRoom6").remove();
+                        document.getElementById("standingTorch1Img").remove();
+                        document.getElementById("standingTorchLighted1Img").style.opacity = 1;
+                        lighting.play();
+                    }, 1000);
+                    break;
+                case "standingTorch3Img":
+                    showMessage(heroSpeech, "You light another candle*", heroAudio);
+                    setTimeout(function () {
+                        lighting.currentTime = 0;
+                        document.getElementById("hiddenRoom3").remove();
+                        document.getElementById("standingTorch3Img").remove();
+                        document.getElementById("standingTorchLighted3Img").style.opacity = 1;
+                        lighting.play();
+                    }, 1000);
                     break;
                 default:
                     break;
